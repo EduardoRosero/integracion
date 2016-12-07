47da5859723f771c731d0f2fde4924c5443a4d03
eduardo <zerhiphop@live.com>
2016-12-07 13:04:32 -0500

    Crear clase funcion

---
+++ src/pkg3_integracionnumerica/Funcion.java
@@ -0,0 +1,14 @@
+package pkg3_integracionnumerica;
+
+/**
+ *
+ * @author Luis Eduardo Rosero Correa
+ */
+public class Funcion{
+
+ public double devuelveFuncion(double x) {
+        double funcion = x * x;
+        return funcion;
+    }
+    
+}
